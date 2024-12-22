bc83894f12be27c02bb7dd26a3b0b62b2adc62cd
Rene Havelka <havelka@outlook.sk>
2024-12-22 09:43:36 +0100

    Úprava názvu súborov s údajmi

--- src/Infrastructure/AccessDataFile.cs
+++ src/Infrastructure/AccessDataFile.cs
@@ -6,22 +6,27 @@ namespace Infrastructure
     {
         internal string GetFileName()
         {
-            var dataFileName = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "NarodeninyMeniny", "birthnamedays.json");
-            var directory = Path.GetDirectoryName(dataFileName);
+			//var dataFileName = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "NarodeninyMeniny", "birthnamedays.json");
+			//var dataFileName = System.IO.Path.Combine(@"C:\Users\Public\NarodeninyMeniny", "birthnamedays.json");
+			var dataFileName = "birthnamedays.json";
+			//var directory = Path.GetDirectoryName(dataFileName);
 
-            if (!Directory.Exists(directory)) Directory.CreateDirectory(directory);
-            if (!Path.Exists(dataFileName)) File.Create(dataFileName).Dispose();
+   //         if (!Directory.Exists(directory)) Directory.CreateDirectory(directory);
+   //         if (!Path.Exists(dataFileName)) File.Create(dataFileName).Dispose();
 
             return dataFileName;
         }
 
         internal string GetCalendarFileName()
         {
-            var calendarFileName = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "NarodeninyMeniny", "Calendar.json");
+			//var calendarFileName = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "NarodeninyMeniny", "Calendar.json");
+			//var calendarFileName = System.IO.Path.Combine(@"C:\Users\Public\NarodeninyMeniny", "Calendar.json");
 
-            if (!Path.Exists(calendarFileName)) return String.Empty;
+			//if (!Path.Exists(calendarFileName)) return String.Empty;
 
-            return calendarFileName;
+			var calendarFileName = "Calendar.json";
+
+			return calendarFileName;
         }
     }
 }
